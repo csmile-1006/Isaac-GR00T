3fb6d596fb27f219fffb2c2da94153708e3a9807
csmile-1006 <changyeon.kim@kaist.ac.kr>
2025-09-09 21:02:50 +0900

    change setup for SIL

--- bash_scripts/2025-09-09_robocasa_singletask_sil.sh
+++ bash_scripts/2025-09-09_robocasa_singletask_sil.sh
@@ -4,23 +4,25 @@
 #SBATCH --error=/home/changyeon/slurm-logs/gr00tn15_rc_single_sil/%j.err   # log                                                                                                   
 #SBATCH --nodes=1            # 노드 1개 사용                                                                                                                 
 #SBATCH --gres=gpu:a6000:4   # GPU 1개 사용                                                                                                                  
-#SBATCH --cpus-per-gpu=16     # GPU당 CPU 사용 수                                                                                                             
-#SBATCH --mem-per-gpu=48G     # GPU당 mem 사용량                                                                                                              
+#SBATCH --cpus-per-gpu=8     # GPU당 CPU 사용 수                                                                                                             
+#SBATCH --mem-per-gpu=32G     # GPU당 mem 사용량                                                                                                              
 #SBATCH --time=72:00:00      # 최대 96시간 실행
 
 TASK_NAME=$1
 NUM_DEMOS=$2
 STEPS=$3
 
-base_path=/home/changyeon/
-source ${base_path}/miniconda3/bin/activate gr00t
-cd ${base_path}/workspace/Isaac-GR00T
+BASE_PATH=/home/changyeon/
+CKPT_PATH=${BASE_PATH}/ckpts/gr00tn15_rbcs_bs32_60k
+source ${BASE_PATH}/miniconda3/bin/activate gr00t
+cd ${BASE_PATH}/workspace/Isaac-GR00T
 
 python scripts/gr00t_finetune.py \
-    --dataset-path "${base_path}/data/robocasa_dataset/${TASK_NAME}_num${NUM_DEMOS}/" \
-                   "${base_path}/gr00tn15_robocasa/rollout_demos/${TASK_NAME}_num${NUM_DEMOS}/" \
+    --base_model_path ${CKPT_PATH} \
+    --dataset-path "${BASE_PATH}/data/robocasa_dataset/${TASK_NAME}_num${NUM_DEMOS}/" \
+                   "${BASE_PATH}/gr00tn15_robocasa/rollout_demos/${TASK_NAME}_num${NUM_DEMOS}/lerobot/" \
     --num-gpus 4 \
-    --output-dir "${base_path}/ckpts/SIL/step${STEPS}_${TASK_NAME}_num${NUM_DEMOS}" \
+    --output-dir "${BASE_PATH}/ckpts/SIL/step${STEPS}_${TASK_NAME}_num${NUM_DEMOS}" \
     --max-steps "${STEPS}" \
     --data-config single_panda_gripper \
     --batch-size 8 \
