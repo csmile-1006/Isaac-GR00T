c0fa8931901eb4d4bbcfced538292fc8ef7c95e5
csmile-1006 <changyeon.kim@kaist.ac.kr>
2025-09-10 02:11:31 +0900

    change hps

--- bash_scripts/2025-09-09_robocasa_singletask_sil.sh
+++ bash_scripts/2025-09-09_robocasa_singletask_sil.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=/home/changyeon/slurm-logs/gr00tn15_rc_single_sil/%j.out  # log                                                                                                   
 #SBATCH --error=/home/changyeon/slurm-logs/gr00tn15_rc_single_sil/%j.err   # log                                                                                                   
 #SBATCH --nodes=1            # 노드 1개 사용                                                                                                                 
-#SBATCH --gres=gpu:a6000:4   # GPU 1개 사용                                                                                                                  
+#SBATCH --gres=gpu:a6000:2   # GPU 1개 사용                                                                                                                  
 #SBATCH --cpus-per-gpu=8     # GPU당 CPU 사용 수                                                                                                             
 #SBATCH --mem-per-gpu=32G     # GPU당 mem 사용량                                                                                                              
 #SBATCH --time=72:00:00      # 최대 96시간 실행
@@ -21,9 +21,9 @@ python scripts/gr00t_finetune.py \
     --base_model_path ${CKPT_PATH} \
     --dataset-path "${BASE_PATH}/data/robocasa_dataset/${TASK_NAME}_num${NUM_DEMOS}/" \
                    "${BASE_PATH}/gr00tn15_robocasa/rollout_demos/${TASK_NAME}_num${NUM_DEMOS}/lerobot/" \
-    --num-gpus 4 \
+    --num-gpus 2 \
     --output-dir "${BASE_PATH}/ckpts/SIL/step${STEPS}_${TASK_NAME}_num${NUM_DEMOS}" \
     --max-steps "${STEPS}" \
     --data-config single_panda_gripper \
-    --batch-size 8 \
+    --batch-size 16 \
     --save-steps 5000
