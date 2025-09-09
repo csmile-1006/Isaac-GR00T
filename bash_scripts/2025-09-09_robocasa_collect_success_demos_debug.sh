ab76e745aafc7ba254648c8b94ad377ff7ff7207
csmile-1006 <changyeon.kim@kaist.ac.kr>
2025-09-09 16:45:18 +0900

    add debugging script

---
+++ bash_scripts/2025-09-09_robocasa_collect_success_demos_debug.sh
@@ -0,0 +1,52 @@
+TASK_NAME=TurnOnMicrowave
+
+ACTION_HORIZON=16
+NUM_ENVS=$1
+NUM_ROLLOUTS=$2
+SERVER=${3:-"alin_slurm"}
+
+if [ "$SERVER" == "rlwrld" ]; then
+    ROOT_PATH=/virtual_lab/sjw_alinlab/changyeon/
+else
+    ROOT_PATH=/home/changyeon/
+fi
+
+source ${ROOT_PATH}/miniconda3/bin/activate gr00t
+cd ${ROOT_PATH}/workspace/Isaac-GR00T
+
+BASE_PATH=${ROOT_PATH}/gr00tn15_robocasa/rollout_demos/
+CKPT_PATH=${ROOT_PATH}/ckpts/gr00tn15_rbcs_bs32_60k
+
+python scripts/collect_demo_robocasa.py \
+    --host localhost \
+    --port 5555 \
+    --data_config single_panda_gripper \
+    --action_horizon ${ACTION_HORIZON} \
+    --embodiment_tag new_embodiment \
+    --model_path ${CKPT_PATH} \
+    --env_name ${TASK_NAME} \
+    --num_episodes ${NUM_ROLLOUTS} \
+    --video_path ${BASE_PATH}/${TASK_NAME}/videos \
+    --collect_data \
+    --reward_shaping \
+    --noise 0.0 \
+    --data_collection_path ${BASE_PATH}/${TASK_NAME}/data \
+    --n_envs ${NUM_ENVS}
+
+cd ${ROOT_PATH}/workspace/robocasa
+OMP_NUM_THREADS=1 MPI_NUM_THREADS=1 MKL_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 python robocasa/scripts/dataset_states_to_obs_multi.py \
+    --dataset ${BASE_PATH}/${TASK_NAME}/data/demo.hdf5 \
+    --camera_width 256 \
+    --camera_height 256 \
+    --generative_textures \
+    --randomize_cameras \
+    --shaped --copy_rewards --copy_dones \
+    --num_procs 4
+
+cd ${ROOT_PATH}/workspace/Isaac-GR00T
+python scripts/convert_hdf5_to_lerobot.py \
+    --src_hdf5_path ${BASE_PATH}/${TASK_NAME}/data/demo_gentex_im256_randcams.hdf5 \
+    --output_path ${BASE_PATH}/${TASK_NAME}/lerobot \
+    --task_name ${TASK_NAME} \
+    --chunks_size 1000 \
+    --num_episodes ${NUM_ROLLOUTS} \
