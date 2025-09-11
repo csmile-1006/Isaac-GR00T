cd29e536c8c64e8dd4d54b986c51d3c7fd77eb5a
csmile-1006 <changyeon.kim@kaist.ac.kr>
2025-09-11 22:00:08 +0900

    additional changes for path name

--- bash_scripts/2025-09-10_robocasa_fql.sh
+++ bash_scripts/2025-09-10_robocasa_fql.sh
@@ -8,14 +8,16 @@ BATCH_SIZE=${7:-32}
 
 BASE_PATH=/home/changyeon/
 CKPT_PATH=${BASE_PATH}/ckpts/gr00tn15_rbcs100_bs32_60k/
+TOTAL_BATCH_SIZE=$((NUM_GPUS * BATCH_SIZE))
+
 source ${BASE_PATH}/miniconda3/bin/activate gr00t
 cd ${BASE_PATH}/workspace/Isaac-GR00T
-RUN_NAME=${TASK_NAME}_num${NUM_DEMOS}_num${NUM_ROLLOUTS}_bs$(($BATCH_SIZE * $NUM_GPUS))_step${STEPS}
+RUN_NAME=FQL_bs${TOTAL_BATCH_SIZE}_steps${RUN_NAME}_demo${NUM_DEMOS}_rollout${NUM_ROLLOUTS}
 python scripts/gr00t_fql_finetune.py \
     --dataset-path ${BASE_PATH}/data/robocasa_dataset/${TASK_NAME}_num${NUM_DEMOS}/ ${BASE_PATH}/gr00tn15_robocasa/rl_rollouts/${TASK_NAME}_num${NUM_ROLLOUTS}/lerobot \
     --base-model-path ${CKPT_PATH} \
     --num-gpus ${NUM_GPUS} \
-    --output-dir ~/ckpts/FQL/${RUN_NAME}/ \
+    --output-dir ${BASE_PATH}/ckpts/${TASK_NAME}/${RUN_NAME}/ \
     --max-steps ${STEPS} \
     --data-config single_panda_gripper_rl \
     --batch-size ${BATCH_SIZE} \
