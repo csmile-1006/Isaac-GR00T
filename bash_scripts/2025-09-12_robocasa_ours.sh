186ae82721747bd9fb2d3d4edf7dfa44a2b62479
csmile-1006 <changyeon.kim@kaist.ac.kr>
2025-09-15 02:36:57 +0900

    change details in ours script

--- bash_scripts/2025-09-12_robocasa_ours.sh
+++ bash_scripts/2025-09-12_robocasa_ours.sh
@@ -4,9 +4,10 @@ NUM_ROLLOUTS=$3
 STEPS=$4
 CRITIC_ACTION_HORIZON=$5
 ALPHA=${6:-3.0}
-DISCOUNT=${7:-0.995}
-NUM_GPUS=${8:-2}
-BATCH_SIZE=${9:-16}
+DISCOUNT1=${7:-0.995}
+DISCOUNT2=${8:-0.995}
+NUM_GPUS=${9:-2}
+BATCH_SIZE=${10:-16}
 
 BASE_PATH=/home/changyeon/
 CKPT_PATH=${BASE_PATH}/ckpts/gr00tn15_rbcs100_bs32_60k/
@@ -14,7 +15,7 @@ TOTAL_BATCH_SIZE=$((NUM_GPUS * BATCH_SIZE))
 
 source ${BASE_PATH}/miniconda3/bin/activate gr00t
 cd ${BASE_PATH}/workspace/Isaac-GR00T
-RUN_NAME=OURS_as${CRITIC_ACTION_HORIZON}_d${DISCOUNT}_al${ALPHA}_bs${TOTAL_BATCH_SIZE}_steps${RUN_NAME}_demo${NUM_DEMOS}_rollout${NUM_ROLLOUTS}
+RUN_NAME=OURS_as${CRITIC_ACTION_HORIZON}_d1${DISCOUNT1}_d2${DISCOUNT2}_al${ALPHA}_bs${TOTAL_BATCH_SIZE}_steps${RUN_NAME}_demo${NUM_DEMOS}_rollout${NUM_ROLLOUTS}
 python scripts/gr00t_ours_finetune.py \
     --dataset-path ${BASE_PATH}/data/robocasa_dataset/${TASK_NAME}_num${NUM_DEMOS}/ ${BASE_PATH}/gr00tn15_robocasa/rollouts/${TASK_NAME}_num${NUM_ROLLOUTS}/lerobot \
     --base-model-path ${CKPT_PATH} \
@@ -26,8 +27,8 @@ python scripts/gr00t_ours_finetune.py \
     --save-steps 10000 \
     --run-name ${RUN_NAME} \
     --critic-action-horizon ${CRITIC_ACTION_HORIZON} \
-    --discount1 ${DISCOUNT} \
-    --discount2 ${DISCOUNT} \
+    --discount1 ${DISCOUNT1} \
+    --discount2 ${DISCOUNT2} \
     --alpha ${ALPHA}
 
     # --lora_rank 64 \
