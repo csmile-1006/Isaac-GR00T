364beed6150a1c7ae6e75df7adfcbce9dd430411
csmile-1006 <changyeon.kim@kaist.ac.kr>
2025-09-20 15:12:18 +0900

    chagne details for franka multitask baseline training

--- bash_scripts/2025-09-20_franka_multitask_qc_critic.sh
+++ bash_scripts/2025-09-20_franka_multitask_qc_critic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash                                                                                                                                                  
-#SBATCH --job-name=critic_grn15_rc_mt
-#SBATCH --output=/home/changyeon/slurm-logs/critic_grn15_rc_mt/%j.out  # log                                                                                                   
-#SBATCH --error=/home/changyeon/slurm-logs/critic_grn15_rc_mt/%j.err   # log                                                                                                   
+#SBATCH --job-name=critic_grn15_frk_qc_mt
+#SBATCH --output=/home/changyeon/slurm-logs/critic_grn15_frk_qc_mt/%j.out  # log                                                                                                   
+#SBATCH --error=/home/changyeon/slurm-logs/critic_grn15_frk_qc_mt/%j.err   # log                                                                                                   
 #SBATCH --nodes=1            # 노드 1개 사용                                                                                                                 
 #SBATCH --exclude=node5,node7
 #SBATCH --gres=gpu:a6000:2   # GPU 1개 사용                                                                                                                  
@@ -12,44 +12,44 @@
 
 NUM_DEMOS=$1
 NUM_ROLLOUTS=$2
-STEPS=$3
-CRITIC_ACTION_HORIZON=1
+ACTOR_CKPT_PATH=$3
+STEPS=$4
+CRITIC_ACTION_HORIZON=$5
 
-EXPECTILE=${4:-0.7}
-NUM_GPUS=${5:-2}
-BATCH_SIZE=${6:-16}
+DISCOUNT1=${6:-0.995}
+DISCOUNT2=${7:-0.995}
+NUM_GPUS=${8:-2}
+BATCH_SIZE=${9:-16}
 
 BASE_PATH=/home/changyeon/
 TOTAL_BATCH_SIZE=$((NUM_GPUS * BATCH_SIZE))
 
 TASK_NAMES=(
-    "CoffeeSetupMug"
-    "PnPCabToCounter"
-    "PnPMicrowaveToCounter"
-    "TurnOffStove"
-    "PnPCounterToMicrowave"
+    "peach_center"
+    "banana_center"
+    "hichew_center"
 )
 
 dataset_path=""
-for TASK in ${TASK_NAMES[@]}; do
-    TASK_NAME=$TASK
-    dataset_path="${dataset_path} ${BASE_PATH}/data/robocasa_dataset/${TASK_NAME}_num${NUM_DEMOS}/ ${BASE_PATH}/gr00tn15_robocasa/rollouts_num${NUM_ROLLOUTS}/${TASK_NAME}/lerobot/"
+for TASK_NAME in ${TASK_NAMES[@]}; do
+    dataset_path="${dataset_path} ${BASE_PATH}/data/franka_dataset/franka_rl_data/rollouts/${TASK_NAME}_num${NUM_ROLLOUTS}/"
+    dataset_path="${dataset_path} ${BASE_PATH}/data/franka_dataset/franka_rl_data/${TASK_NAME}_num${NUM_DEMOS}/"
 done
 
 source ${BASE_PATH}/miniconda3/bin/activate gr00t
 cd ${BASE_PATH}/workspace/Isaac-GR00T
-RUN_NAME=IQL_Critic_as${CRITIC_ACTION_HORIZON}_e${EXPECTILE}_bs${TOTAL_BATCH_SIZE}_steps${RUN_NAME}_dm${NUM_DEMOS}_roll${NUM_ROLLOUTS}
-python scripts/gr00t_iql_critic_finetune.py \
+RUN_NAME=QC_Critic_as${CRITIC_ACTION_HORIZON}_bs${TOTAL_BATCH_SIZE}_steps${STEPS}_dm${NUM_DEMOS}_roll${NUM_ROLLOUTS}
+python scripts/gr00t_fql_finetune.py \
+    --base-model-path ${ACTOR_CKPT_PATH} \
     --dataset-path ${dataset_path} \
     --num-gpus ${NUM_GPUS} \
-    --output-dir ${BASE_PATH}/ckpts/multiple/${RUN_NAME}/ \
+    --output-dir ${BASE_PATH}/franka_ckpts/multiple/${RUN_NAME}/ \
     --max-steps ${STEPS} \
-    --data-config single_panda_gripper_rl \
+    --data-config droid_panda_wrist_gripper_rl \
     --batch-size ${BATCH_SIZE} \
     --save-steps 5000 \
     --run-name ${RUN_NAME} \
     --critic-action-horizon ${CRITIC_ACTION_HORIZON} \
-    --expectile ${EXPECTILE} \
-
-    # --lora_rank 64 \
-    # --lora_alpha 128
+    --discount1 ${DISCOUNT1} \
+    --discount2 ${DISCOUNT2} \
+    --video_backend torchvision_av
